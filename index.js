e3d4b58a1fd524b177bebdd919f96db7dc68fc3d
Macbook Kantor 202165 <adityasudyana@gmail.com>
2023-09-13 15:57:02 +0700

    puppeteer no gui 7

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const moment = require('moment')
 const client = new Client({
     puppeteer: {
         headless: true,
-        // executablePath: '/usr/bin/chromium-browser',
+        executablePath: '/usr/bin/chromium-browser',
         ignoreDefaultArgs: [
             '--disable-extensions'
         ],
